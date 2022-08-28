b378849962af38325415a66c74f2bba05131bbad
ansk1d <anasalkhaled@aol.com>
2022-08-28 11:32:51 +0300

    edit!

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 
 
 export default (props) => {
     const [poke, setPoke] = useState([]);
 
-    const feachPokemon = () => {
-        axios.get('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
-        .then(response => setPoke(response.data.results));
+    function feachPokemon(){
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+        .then(response => response.json())
+            .then(response => setPoke(response.results))
     };
-
-
+    
     return (
         
         <div className="nav">
             <button onClick={feachPokemon}>Feach Pokemon</button>
-
             <div>
             {poke.length > 0 && poke.map((poke, i)=>{
                 return (<div key={i}>{poke.name}</div>)
